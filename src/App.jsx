542915f5a72c1c449a8a47f43a02c57b4ce5c8a2
Joshua <izeremanzijosue2@gmail.com>
2025-08-12 14:39:06 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='text-7xl font-bold pb-8'>welcome to unwind hotel booking</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, vero.</p>
+    </>
+  )
+}
+
+export default App
